c169e6a0773e2d9959353c267675027d9312aa40
Andrew Hudson <ahudson@gmail.com>
2020-11-21 18:23:42 +0000

    start work on getting all light statuses

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+# House Control via Next and API routes with Sonos and Hue
+
+## TODO
+
+- [ ] Discovery of all lights
+- [ ] Status of all lights
+- [ ] Turn all lights off/on
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
+
 make an .env file with the following
 
 ```bash
@@ -12,3 +21,15 @@ SONOS_KITCHEN_EATING_IP=""
 SONOS_BEDROOM_IP=""
 SONOS_LOUNGE_IP=""
 ```
+
+## How to run on Raspberry Pi
+
+- run `npm run build`
+- run `npm run start` to run the server
+
+### PM2
+
+If you want to run this continually you'll need `pm2`.
+
+- `pm2 start npm --name "Next App" -- start`
+- if you want to set pm2 to start when the pi boots follow [this tutorial](https://medium.com/@andrew.nease.code/set-up-a-self-booting-node-js-eb56ebd05549)
--- pages/api/hue/getall-lights.js
+++ pages/api/hue/getall-lights.js
@@ -1,28 +1,11 @@
-const v3 = require("node-hue-api").v3;
-const LightState = v3.lightStates.LightState;
+const { getAllLights } = require("../utils/lights");
 
-const { HUE_BRIDGE_USER, HUE_BRIDGE_USER_CLIENT_KEY } = process.env;
+export default async function handler(req, res) {
+	const lights = await getAllLights();
 
-const USERNAME = HUE_BRIDGE_USER;
-
-v3.discovery
-	.nupnpSearch()
-	.then((searchResults) => {
-		const host = searchResults[0].ipaddress;
-		return v3.api.createLocal(host).connect(USERNAME);
-	})
-	.then((api) => {
-		return api.lights.getAll();
-	})
-	.then((allLights) => {
-		// Display the details of the lights we got back
-		console.log(JSON.stringify(allLights, null, 2));
-
-		// Iterate over the light objects showing details
-		allLights.forEach((light) => {
-			console.log(light.toStringDetailed());
-		});
-	})
-	.catch((err) => {
-		console.error(err);
+	res.statusCode = 200;
+	res.json({
+		name: "Hue getall-lights",
+		lights: lights.map((light) => light.id),
 	});
+}
