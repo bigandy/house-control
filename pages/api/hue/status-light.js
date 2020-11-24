14be4d8d3028d4f7a83d9b64cbddaa22b6bfa05b
Andrew Hudson <andrew@ventureharbour.com>
2020-11-24 19:34:38 +0000

    re-write status-light

--- pages/api/hue/status-light.js
+++ pages/api/hue/status-light.js
@@ -1,36 +1,22 @@
 const v3 = require("node-hue-api").v3;
 const LightState = v3.lightStates.LightState;
 
-const { OFFICE_LIGHT } = require("../utils/hue");
+const { statusLight } = require("../utils/hue");
 
-const { HUE_BRIDGE_USER, HUE_BRIDGE_USER_CLIENT_KEY } = process.env;
+export default async function handler(req, res) {
+  try {
+    const { state } = await statusLight();
 
-const USERNAME = HUE_BRIDGE_USER,
-	LIGHT_ID = OFFICE_LIGHT;
-
-export default function handler(req, res) {
-	v3.discovery
-		.nupnpSearch()
-		.then((searchResults) => {
-			const host = searchResults[0].ipaddress;
-			return v3.api.createLocal(host).connect(USERNAME);
-		})
-		.then(async (api) => {
-			await api.lights.getLightState(LIGHT_ID).then((state) => {
-				// Display the state of the light
-				console.log(JSON.stringify(state, null, 2));
-
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.end(
-					JSON.stringify({
-						query: req.query,
-						state,
-					})
-				);
-			});
-		})
-		.catch((e) => {
-			throw new Error("lights failed");
-		});
+    res.statusCode = 200;
+    res.json({
+      name: "Hue status-room",
+      state,
+    });
+  } catch (error) {
+    res.statusCode = 400;
+    res.json({
+      name: "Hue status-room-error",
+      error,
+    });
+  }
 }
--- pages/api/utils/hue.js
+++ pages/api/utils/hue.js
@@ -5,134 +5,141 @@ const { HUE_BRIDGE_USER, HUE_BRIDGE_USER_CLIENT_KEY } = process.env;
 
 const USERNAME = HUE_BRIDGE_USER;
 
-const { OFFICE_LIGHT } = require("./hue");
+const OFFICE_LIGHT = 5;
 
 const getAllLights = async () => {
-	const statuses = await v3.discovery
-		.nupnpSearch()
-		.then((searchResults) => {
-			const host = searchResults[0].ipaddress;
-			return v3.api.createLocal(host).connect(USERNAME);
-		})
-		.then((api) => {
-			return api.lights.getAll();
-		})
-		.then((allLights) => {
-			const lights = [];
-			// Iterate over the light objects showing details
-			allLights.forEach((light) => {
-				const { id, name, state } = light;
-				lights.push({ id, name, state });
-			});
+  const statuses = await v3.discovery
+    .nupnpSearch()
+    .then((searchResults) => {
+      const host = searchResults[0].ipaddress;
+      return v3.api.createLocal(host).connect(USERNAME);
+    })
+    .then((api) => {
+      return api.lights.getAll();
+    })
+    .then((allLights) => {
+      const lights = [];
+      // Iterate over the light objects showing details
+      allLights.forEach((light) => {
+        const { id, name, state } = light;
+        lights.push({ id, name, state });
+      });
 
-			return lights;
-		})
-		.catch((err) => {
-			console.error(err);
-		});
-	return statuses;
+      return lights;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  return statuses;
 };
 
 const handleAll = async (method = "off") => {
-	console.log("method", method);
+  console.log("method", method);
 
-	const devices = await getAllLights();
+  const devices = await getAllLights();
 
-	await devices.reduce(async (previousPromise, nextID) => {
-		await previousPromise;
+  await devices.reduce(async (previousPromise, nextID) => {
+    await previousPromise;
 
-		if (method === "off") {
-			return offLight(nextID.id);
-		} else if (method === "on") {
-			return onLight(nextID.id);
-		} else if (method === "toggle") {
-			return toggleLight(nextID.id);
-		} else {
-			throw new Error("No known light method");
-		}
-	}, Promise.resolve());
-	const statuses = await getAllLights();
+    if (method === "off") {
+      return offLight(nextID.id);
+    } else if (method === "on") {
+      return onLight(nextID.id);
+    } else if (method === "toggle") {
+      return toggleLight(nextID.id);
+    } else {
+      throw new Error("No known light method");
+    }
+  }, Promise.resolve());
+  const statuses = await getAllLights();
 
-	return statuses;
+  return statuses;
 };
 
 const toggleLight = async (lightId = OFFICE_LIGHT) => {
-	return v3.discovery
-		.nupnpSearch()
-		.then((searchResults) => {
-			const host = searchResults[0].ipaddress;
-			return v3.api.createLocal(host).connect(USERNAME);
-		})
-		.then(async (api) => {
-			// Using a basic object to set the state
-			const state = await api.lights.getLightState(lightId);
+  return v3.discovery
+    .nupnpSearch()
+    .then((searchResults) => {
+      const host = searchResults[0].ipaddress;
+      return v3.api.createLocal(host).connect(USERNAME);
+    })
+    .then(async (api) => {
+      // Using a basic object to set the state
+      const state = await api.lights.getLightState(lightId);
 
-			const result = await api.lights.setLightState(lightId, {
-				on: !state.on,
-			});
-			return result;
-		})
-		.catch((e) => {
-			throw new Error("lights failed");
-			res.send({
-				statusCode: 400,
-				body: JSON.stringify(e),
-			});
-		});
+      const result = await api.lights.setLightState(lightId, {
+        on: !state.on,
+      });
+      return result;
+    })
+    .catch((e) => {
+      throw new Error("lights failed");
+      res.send({
+        statusCode: 400,
+        body: JSON.stringify(e),
+      });
+    });
 };
 
 const offLight = async (lightId = OFFICE_LIGHT) => {
-	return v3.discovery
-		.nupnpSearch()
-		.then((searchResults) => {
-			const host = searchResults[0].ipaddress;
-			return v3.api.createLocal(host).connect(USERNAME);
-		})
-		.then(async (api) => {
-			// Using a basic object to set the state
-			const state = await api.lights.getLightState(lightId);
+  return v3.discovery
+    .nupnpSearch()
+    .then((searchResults) => {
+      const host = searchResults[0].ipaddress;
+      return v3.api.createLocal(host).connect(USERNAME);
+    })
+    .then(async (api) => {
+      // Using a basic object to set the state
+      const state = await api.lights.getLightState(lightId);
 
-			const result = await api.lights.setLightState(lightId, {
-				on: false,
-			});
-			return result;
-		})
-		.catch((e) => {
-			throw new Error("lights failed");
-			res.send({
-				statusCode: 400,
-				body: JSON.stringify(e),
-			});
-		});
+      const result = await api.lights.setLightState(lightId, {
+        on: false,
+      });
+      return result;
+    })
+    .catch((e) => {
+      throw new Error("lights failed");
+      res.send({
+        statusCode: 400,
+        body: JSON.stringify(e),
+      });
+    });
 };
 
 const onLight = async (lightId = OFFICE_LIGHT) => {
-	return v3.discovery
-		.nupnpSearch()
-		.then((searchResults) => {
-			const host = searchResults[0].ipaddress;
-			return v3.api.createLocal(host).connect(USERNAME);
-		})
-		.then(async (api) => {
-			const result = await api.lights.setLightState(lightId, {
-				on: true,
-			});
-			return result;
-		})
-		.catch((e) => {
-			throw new Error("lights failed");
-			res.send({
-				statusCode: 400,
-				body: JSON.stringify(e),
-			});
-		});
+  return v3.discovery
+    .nupnpSearch()
+    .then((searchResults) => {
+      const host = searchResults[0].ipaddress;
+      return v3.api.createLocal(host).connect(USERNAME);
+    })
+    .then(async (api) => {
+      const result = await api.lights.setLightState(lightId, {
+        on: true,
+      });
+      return result;
+    })
+    .catch((e) => {
+      throw new Error("lights failed");
+      res.send({
+        statusCode: 400,
+        body: JSON.stringify(e),
+      });
+    });
+};
+
+const statusLight = async (lightId = OFFICE_LIGHT) => {
+  const allLights = await getAllLights();
+  const light = allLights.find((light) => light.id === lightId);
+  return light;
 };
 
 module.exports = {
-	OFFICE_LIGHT: 5,
-	getAllLights,
-	toggleLight,
-	onLight,
-	offLight,
-	handleAll,
+  OFFICE_LIGHT,
+  getAllLights,
+  toggleLight,
+  onLight,
+  offLight,
+  statusLight,
+  handleAll,
 };
