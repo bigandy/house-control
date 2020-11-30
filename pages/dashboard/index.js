ac8c7292056fcf7f9d74248de3106230a73b49b2
Andrew Hudson <andrew@ventureharbour.com>
2020-11-30 13:53:34 +0000

    remove padStart on the hour

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -19,8 +19,8 @@ const Clock = ({ minutes, hours }) => {
 
   return (
     <div>
-      {String(hours > 12 ? hours - 12 : hours).padStart(2, "0")}:
-      {String(minutes).padStart(2, "0")} {isPm ? "pm" : "am"}
+      {hours > 12 ? hours - 12 : hours}:{String(minutes).padStart(2, "0")}{" "}
+      {isPm ? "pm" : "am"}
     </div>
   );
 };
