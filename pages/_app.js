b3c4ca0ee9067db007ed96ae43f7060c2a141003
Andrew Hudson <andrew@ventureharbour.com>
2021-02-17 19:08:11 +0000

    continue on plugs page

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
--- pages/plugs/index.js
+++ pages/plugs/index.js
@@ -8,29 +8,20 @@ import styles from "styles/Home.module.scss";
 import { sonosRooms } from "utils/sonos";
 import fetch from "node-fetch";
 
-const RoomSelector = ({ selected, handleChange }) => {
-  return (
-    <select onChange={handleChange} value={selected}>
-      {["2", "3", "4", "office"].map((room, index) => {
-        return (
-          <option value={room} key={`select-${index}`}>
-            {room}
-          </option>
-        );
-      })}
-    </select>
-  );
-};
+const plugs = ["2", "3", "4", "office"];
 
 export default function Home() {
   const [room, setRoom] = useState("4");
-  const [status, setStatus] = useState(false);
+  //   const [status, setStatus] = useState(plugs.map(() => false));
+
+  // AHTODO Keep track of on/off status using the API.
 
   useEffect(async () => {
     const plugs = await await fetch(`/api/plug/statuses`)
       .then((res) => res.json())
       .then((json) => {
-        setStatus(json.plugs.find((plug) => plug.name === room).status);
+        console.log(status);
+        // setStatus(json.plugs.findIndex((plug) => plug.name === room).status);
       })
       .catch((e) => console.error(e));
   }, [room]);
@@ -39,13 +30,19 @@ export default function Home() {
     setRoom(e.target.value);
   };
 
-  const togglePlug = async () => {
-    console.log(room);
-
-    await fetch(`/api/plug/plug-toggle/?room=${room}`)
+  const togglePlug = async (plug) => {
+    await fetch(`/api/plug/plug-toggle/?room=${plug}`)
       .then((res) => res.json())
       .then((json) => setStatus(json.statusOut))
       .catch((e) => console.error(e));
+  };
+
+  const offAllPlugs = async (plug) => {
+    await fetch(`/api/plug/plug-all-off`).catch((e) => console.error(e));
+  };
+
+  const onAllPlugs = async (plug) => {
+    await fetch(`/api/plug/plug-all-on`).catch((e) => console.error(e));
   };
 
   return (
@@ -53,12 +50,14 @@ export default function Home() {
       <Head>
         <title>Plug</title>
       </Head>
-      <div className={styles.container}>
-        <RoomSelector handleChange={handleRoomChange} selected={room} />
 
-        <button onClick={togglePlug}>
-          {room} - {status ? "Off" : "On"}
-        </button>
+      <div className={styles.container}>
+        <button onClick={() => offAllPlugs()}>Off All</button>
+        <button onClick={() => onAllPlugs()}>On All</button>
+
+        {plugs.map((plug) => {
+          return <button onClick={() => togglePlug(plug)}>{plug}</button>;
+        })}
       </div>
     </DefaultLayout>
   );
