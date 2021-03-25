aef6d7945898c5da8fef10da0f4fc9773d0a473e
Andrew Hudson <ahudson@gmail.com>
2021-03-25 20:54:57 +0000

    use localStorage to remember the room across pages and on reload

--- pages/index.js
+++ pages/index.js
@@ -8,98 +8,110 @@ import styles from "styles/Home.module.scss";
 import { sonosRooms } from "utils/sonos";
 
 const RoomSelector = ({ selected, handleChange }) => {
-  return (
-    <select onChange={handleChange}>
-      {sonosRooms.map((room, index) => {
-        return (
-          <option value={room.id} key={`select-${index}`}>
-            {room.text}
-          </option>
-        );
-      })}
-    </select>
-  );
+	return (
+		<select onChange={handleChange} value={selected}>
+			{sonosRooms.map((room, index) => {
+				return (
+					<option value={room.id} key={`select-${index}`}>
+						{room.text}
+					</option>
+				);
+			})}
+		</select>
+	);
 };
 
 export default function HomePage() {
-  const [room, setRoom] = useState("lounge");
-  const [musicPlaying, setMusicPlaying] = useState(false);
+	const [selectedRoom, setSelectedRoom] = useState("bedroom");
+	const [musicPlaying, setMusicPlaying] = useState(false);
 
-  const [lightsOn, setLightsOn] = useState(false);
+	const [lightsOn, setLightsOn] = useState(false);
 
-  const toggleLight = async () => {
-    await fetch("/api/hue/toggle-light")
-      .then((res) => res.json())
-      .then((json) => {
-        setLightsOn((prevState) => !prevState);
-      })
-      .catch((e) => console.error(e));
-  };
+	useEffect(() => {
+		const room = localStorage.getItem("room");
+		console.log({ room });
+		if (room) {
+			setSelectedRoom(room);
+		}
+	}, []);
 
-  useEffect(async () => {
-    await fetch(`/api/sonos/status-room/?room=${room}`)
-      .then((res) => res.json())
-      .then((json) => setMusicPlaying(json.status))
-      .catch((e) => console.error(e));
-  }, [room]);
+	console.log(selectedRoom);
 
-  useEffect(async () => {
-    await fetch(`/api/hue/status-light`)
-      .then((res) => res.json())
-      .then((json) => setLightsOn(json.state.on))
-      .catch((e) => console.error(e));
-  }, []);
+	const toggleLight = async () => {
+		await fetch("/api/hue/toggle-light")
+			.then((res) => res.json())
+			.then((json) => {
+				setLightsOn((prevState) => !prevState);
+			})
+			.catch((e) => console.error(e));
+	};
 
-  const toggleMusic = async () => {
-    await fetch(`/api/sonos/toggle-room/?room=${room}`)
-      .then((res) => res.json())
-      .then((json) => setMusicPlaying(json.status))
-      .catch((e) => console.error(e));
-  };
+	useEffect(async () => {
+		await fetch(`/api/sonos/status-room/?room=${selectedRoom}`)
+			.then((res) => res.json())
+			.then((json) => setMusicPlaying(json.status))
+			.catch((e) => console.error(e));
+	}, [selectedRoom]);
 
-  const handleRoomChange = (e) => {
-    setRoom(e.target.value);
-  };
+	useEffect(async () => {
+		await fetch(`/api/hue/status-light`)
+			.then((res) => res.json())
+			.then((json) => setLightsOn(json.state.on))
+			.catch((e) => console.error(e));
+	}, []);
 
-  const turnOffAllSonos = async () => {
-    await fetch(`/api/sonos/pause-all`).catch((e) => console.error(e));
-  };
+	const toggleMusic = async () => {
+		await fetch(`/api/sonos/toggle-room/?room=${selectedRoom}`)
+			.then((res) => res.json())
+			.then((json) => setMusicPlaying(json.status))
+			.catch((e) => console.error(e));
+	};
 
-  const turnOffAllHue = async () => {
-    await fetch(`/api/hue/off-all`).catch((e) => console.error(e));
-  };
+	const handleRoomChange = (e) => {
+		const { value } = e.target;
+		setSelectedRoom(value);
+		localStorage.setItem("room", value);
+	};
 
-  const turnOffAllPlugs = async () => {
-    await fetch(`/api/plug/plug-all-off`).catch((e) => console.error(e));
-  };
+	const turnOffAllSonos = async () => {
+		await fetch(`/api/sonos/pause-all`).catch((e) => console.error(e));
+	};
 
-  const turnOffEverything = async () => {
-    await turnOffAllHue();
-    await turnOffAllSonos();
-    await turnOffAllPlugs();
-  };
+	const turnOffAllHue = async () => {
+		await fetch(`/api/hue/off-all`).catch((e) => console.error(e));
+	};
 
-  return (
-    <DefaultLayout>
-      <Head>
-        <title>Homepage</title>
-      </Head>
-      <div className={styles.container}>
-        <RoomSelector handleChange={handleRoomChange} selected={room} />
+	const turnOffAllPlugs = async () => {
+		await fetch(`/api/plug/plug-all-off`).catch((e) => console.error(e));
+	};
 
-        <button onClick={toggleLight}>
-          Turn Office Lights {lightsOn ? "Off" : "On"}
-        </button>
-        <button onClick={toggleMusic}>
-          Turn Music in <em>{room}</em>{" "}
-          {musicPlaying === "paused" || musicPlaying === "stopped"
-            ? "On"
-            : "Off"}
-        </button>
-        <button onClick={turnOffAllSonos}>Turn All SONOS off</button>
-        <button onClick={turnOffAllHue}>Turn All HUE off</button>
-        <button onClick={turnOffEverything}>Turn Everything off</button>
-      </div>
-    </DefaultLayout>
-  );
+	const turnOffEverything = async () => {
+		await turnOffAllHue();
+		await turnOffAllSonos();
+		await turnOffAllPlugs();
+	};
+
+	return (
+		<DefaultLayout>
+			<Head>
+				<title>Homepage</title>
+			</Head>
+			<div className={styles.container}>
+				<RoomSelector handleChange={handleRoomChange} selected={selectedRoom} />
+
+				<button onClick={toggleLight}>
+					Turn Office Lights {lightsOn ? "Off" : "On"}
+				</button>
+				<button onClick={toggleMusic}>
+					Turn Music in <em>{selectedRoom}</em>{" "}
+					{musicPlaying === "paused" || musicPlaying === "stopped"
+						? "On"
+						: "Off"}
+				</button>
+				<button onClick={turnOffAllSonos}>Turn All SONOS off</button>
+				<button onClick={turnOffAllHue}>Turn All HUE off</button>
+				<button onClick={turnOffEverything}>Turn Everything off</button>
+			</div>
+		</DefaultLayout>
+	);
 }
--- pages/music/room/index.js
+++ pages/music/room/index.js
@@ -12,191 +12,202 @@ const pageTitle = "Music";
 const { getFavorites } = require("pages/api/utils/sonos");
 
 export default function MusicRoomPage({ favorites }) {
-  const [musicPlaying, setMusicPlaying] = useState(false);
+	const [musicPlaying, setMusicPlaying] = useState(false);
 
-  const [selectedRoom, setSelectedRoom] = useState("bedroom");
+	const [selectedRoom, setSelectedRoom] = useState("bedroom");
 
-  const [currentFavorite, setCurrentFavorite] = useState(null);
+	const [currentFavorite, setCurrentFavorite] = useState(null);
 
-  useEffect(async () => {
-    await fetch(`/api/sonos/status-all`)
-      .then((res) => res.json())
-      .then((json) => {
-        const roomsObj = {};
+	useEffect(() => {
+		const room = localStorage.getItem("room");
 
-        json.statuses.forEach((room) => {
-          roomsObj[room.room] =
-            room.state !== "paused" && room.state !== "stopped";
-        });
+		console.log({ room });
+		if (room) {
+			setSelectedRoom(room);
+		}
+	}, []);
 
-        setMusicPlaying(roomsObj);
-      })
-      .catch((e) => console.error(e));
-  }, []);
+	useEffect(async () => {
+		await fetch(`/api/sonos/status-all`)
+			.then((res) => res.json())
+			.then((json) => {
+				const roomsObj = {};
 
-  useEffect(async () => {
-    try {
-      const statuses = await fetch(`/api/sonos/status-all`)
-        .then((res) => res.json())
-        .then((json) => {
-          const roomsObj = {};
-          json.statuses.forEach(
-            (room) =>
-              (roomsObj[room.room] =
-                room.state !== "paused" && room.state !== "stopped")
-          );
-          setMusicPlaying(roomsObj);
-        })
-        .catch((e) => console.error(e));
-    } catch (error) {
-      console.error("error in useEffect", error);
-    }
-  }, [selectedRoom]);
+				json.statuses.forEach((room) => {
+					roomsObj[room.room] =
+						room.state !== "paused" && room.state !== "stopped";
+				});
 
-  const playFavorite = async () => {
-    // update state fast.
-    // setPlaying((prevState) => !prevState);
-    await fetch(
-      `/api/sonos/play-favorite?favorite=${JSON.stringify(
-        currentFavorite
-      )}&room=${selectedRoom}`
-    )
-      .then((res) => res.json())
-      .then(({ status }) => {
-        // if the status is different, need to update the state then.
-        // setPlaying(status === "transitioning");
-      })
-      .catch((e) => console.error(e));
-  };
+				setMusicPlaying(roomsObj);
+			})
+			.catch((e) => console.error(e));
+	}, []);
 
-  const toggleRoom = async () => {
-    console.log("I want to toggle room");
+	useEffect(async () => {
+		try {
+			const statuses = await fetch(`/api/sonos/status-all`)
+				.then((res) => res.json())
+				.then((json) => {
+					const roomsObj = {};
+					json.statuses.forEach(
+						(room) =>
+							(roomsObj[room.room] =
+								room.state !== "paused" && room.state !== "stopped")
+					);
+					setMusicPlaying(roomsObj);
+				})
+				.catch((e) => console.error(e));
+		} catch (error) {
+			console.error("error in useEffect", error);
+		}
+	}, [selectedRoom]);
 
-    // if there is a favorite selected, want to play that.
-    await toggleMusic();
-  };
+	const playFavorite = async () => {
+		// update state fast.
+		// setPlaying((prevState) => !prevState);
+		await fetch(
+			`/api/sonos/play-favorite?favorite=${JSON.stringify(
+				currentFavorite
+			)}&room=${selectedRoom}`
+		)
+			.then((res) => res.json())
+			.then(({ status }) => {
+				// if the status is different, need to update the state then.
+				// setPlaying(status === "transitioning");
+			})
+			.catch((e) => console.error(e));
+	};
 
-  const playFavoriteNext = async () => {
-    setCurrentFavorite(null);
-    setMusicPlaying((prevState) => {
-      return {
-        ...prevState,
-        [selectedRoom]: false,
-      };
-    });
-    await playFavorite();
+	const toggleRoom = async () => {
+		await toggleMusic();
+	};
 
-    setMusicPlaying((prevState) => {
-      return {
-        ...prevState,
-        [selectedRoom]: true,
-      };
-    });
-  };
+	const playFavoriteNext = async () => {
+		setCurrentFavorite(null);
+		setMusicPlaying((prevState) => {
+			return {
+				...prevState,
+				[selectedRoom]: false,
+			};
+		});
+		await playFavorite();
 
-  const toggleMusic = async () => {
-    console.log("toggleMusic room", selectedRoom);
-    await fetch(`/api/sonos/toggle-room/?room=${selectedRoom}`)
-      .then((res) => res.json())
-      .then((json) => {
-        setMusicPlaying((prevState) => {
-          return {
-            ...prevState,
-            [selectedRoom]: json.status === "transitioning",
-          };
-        });
-      })
-      // .then((json) => {
-      //   setMusicPlaying((prevState) => {
-      //     return {
-      //       ...prevState,
-      //       [room]: json.status === "transitioning",
-      //     };
-      //   });
-      // })
-      .catch((e) => console.error(e));
-  };
+		setMusicPlaying((prevState) => {
+			return {
+				...prevState,
+				[selectedRoom]: true,
+			};
+		});
+	};
 
-  // AHTODO:
-  //   1. How to persist state to localStorage so that when I refresh the page, the room is pre-selected
-  //   2. How to list the favorites from Sonos? Could be Spotify Playlists or something too
+	const toggleMusic = async () => {
+		console.log("toggleMusic room", selectedRoom);
+		await fetch(`/api/sonos/toggle-room/?room=${selectedRoom}`)
+			.then((res) => res.json())
+			.then((json) => {
+				setMusicPlaying((prevState) => {
+					return {
+						...prevState,
+						[selectedRoom]: json.status === "transitioning",
+					};
+				});
+			})
+			// .then((json) => {
+			//   setMusicPlaying((prevState) => {
+			//     return {
+			//       ...prevState,
+			//       [room]: json.status === "transitioning",
+			//     };
+			//   });
+			// })
+			.catch((e) => console.error(e));
+	};
 
-  const icon = useMemo(() => {
-    const playIcon = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26";
-    const pauseIcon =
-      "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28";
+	// AHTODO:
+	//   1. How to persist state to localStorage so that when I refresh the page, the room is pre-selected
+	//   2. How to list the favorites from Sonos? Could be Spotify Playlists or something too
 
-    if (musicPlaying[selectedRoom]) {
-      return playIcon;
-    }
-    return pauseIcon;
-  }, [musicPlaying, selectedRoom]);
+	const icon = useMemo(() => {
+		const playIcon = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26";
+		const pauseIcon =
+			"M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28";
 
-  return (
-    <DefaultLayout title="Music">
-      <div className="play-wrapper">
-        <button
-          onClick={() => toggleRoom()}
-          className={classnames({
-            active: musicPlaying[selectedRoom],
-          })}
-        >
-          <svg width="100px" height="100px" viewBox="0 0 36 36">
-            <path d={icon} />
-          </svg>
-        </button>
+		if (musicPlaying[selectedRoom]) {
+			return playIcon;
+		}
+		return pauseIcon;
+	}, [musicPlaying, selectedRoom]);
 
-        {currentFavorite && (
-          <button onClick={playFavoriteNext}>
-            Play {currentFavorite?.title}
-          </button>
-        )}
-      </div>
+	const handleRoomChange = (room) => {
+		setSelectedRoom(room);
+		localStorage.setItem("room", room);
+	};
 
-      <h2>Selected Room is : {selectedRoom}</h2>
-      <div className={styles.container}>
-        {["bedroom", "lounge", "kitchen", "kitchen-eating"].map((room) => {
-          return (
-            <label
-              htmlFor={room}
-              key={room}
-              className={classnames({
-                active: room === selectedRoom,
-              })}
-            >
-              <input
-                id={room}
-                type="radio"
-                checked={room === selectedRoom}
-                onChange={() => setSelectedRoom(room)}
-              />
-              {room}
-            </label>
-          );
-        })}
+	return (
+		<DefaultLayout title="Music">
+			<div className="play-wrapper">
+				<button
+					onClick={() => toggleRoom()}
+					className={classnames({
+						active: musicPlaying[selectedRoom],
+					})}
+				>
+					<svg width="100px" height="100px" viewBox="0 0 36 36">
+						<path d={icon} />
+					</svg>
+				</button>
 
-        {favorites &&
-          Object.keys(favorites).map((favorite) => {
-            return (
-              <button
-                onClick={() => setCurrentFavorite(favorites[favorite])}
-                key={favorite}
-                className={classnames("button-music", {
-                  active: currentFavorite?.title === favorites[favorite].title,
-                })}
-              >
-                {favorites[favorite].title}
-              </button>
-            );
-          })}
-      </div>
-    </DefaultLayout>
-  );
+				{currentFavorite && (
+					<button onClick={playFavoriteNext}>
+						Play {currentFavorite?.title}
+					</button>
+				)}
+			</div>
+
+			<h2>Selected Room is : {selectedRoom}</h2>
+			<div className={styles.container}>
+				{["bedroom", "lounge", "kitchen", "kitchen-eating"].map((room) => {
+					return (
+						<label
+							htmlFor={room}
+							key={room}
+							className={classnames({
+								active: room === selectedRoom,
+							})}
+						>
+							<input
+								id={room}
+								type="radio"
+								checked={room === selectedRoom}
+								onChange={() => handleRoomChange(room)}
+							/>
+							{room}
+						</label>
+					);
+				})}
+
+				{favorites &&
+					Object.keys(favorites).map((favorite) => {
+						return (
+							<button
+								onClick={() => setCurrentFavorite(favorites[favorite])}
+								key={favorite}
+								className={classnames("button-music", {
+									active: currentFavorite?.title === favorites[favorite].title,
+								})}
+							>
+								{favorites[favorite].title}
+							</button>
+						);
+					})}
+			</div>
+		</DefaultLayout>
+	);
 }
 
 export async function getStaticProps(context) {
-  const { formattedFavorites: favorites } = await getFavorites("bedroom");
-  return {
-    props: { favorites }, // will be passed to the page component as props
-  };
+	const { formattedFavorites: favorites } = await getFavorites("bedroom");
+	return {
+		props: { favorites }, // will be passed to the page component as props
+	};
 }
