56556c5243ac541aff0d68f487c27791cac15f9a
Andrew Hudson <andrew@ventureharbour.com>
2021-03-29 10:20:26 +0100

    spotify stuff

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,23 +2,39 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
 export default NextAuth({
-	// Configure one or more authentication providers
-	providers: [
-		Providers.Spotify({
-			clientId: process.env.SPOTIFY_CLIENT_ID,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-			// profile(profile) {
-			// 	return {
-			// 		id: profile.id,
-			// 		name: profile.display_name,
-			// 		email: profile.email,
-			// 		image: profile.images?.[0]?.url,
-			// 	};
-			// },
-		}),
-		// ...add more providers here
-	],
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Spotify({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      profile(profile) {
+        console.log({ profile });
+        return {
+          id: profile.id,
+          name: profile.display_name,
+          email: profile.email,
+          image: profile.images?.[0]?.url,
+        };
+      },
+    }),
+    // ...add more providers here
+  ],
 
-	// A database is optional, but required to persist accounts in a database
-	//   database: process.env.DATABASE_URL,
+  session: {
+    jwt: true,
+  },
+
+  callbacks: {
+    async jwt(token, _, account) {
+      if (account) {
+        token.id = account.id;
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    async session(session, user) {
+      session.user = user;
+      return session;
+    },
+  },
 });
--- pages/spotify/index.tsx
+++ pages/spotify/index.tsx
@@ -13,24 +13,44 @@ import { useRouter } from "next/router";
 const pageTitle = "Music";
 
 export default function SpotifyPage() {
-	const [session, loading] = useSession();
+  const [session, loading, ...rest] = useSession();
 
-	return (
-		<DefaultLayout title="Spotify">
-			<div className={styles.container}>
-				{!session && (
-					<>
-						Not signed in <br />
-						<button onClick={() => signIn()}>Sign in</button>
-					</>
-				)}
-				{session && (
-					<>
-						Signed in as {session.user.name} <br />
-						<button onClick={() => signOut()}>Sign out</button>
-					</>
-				)}
-			</div>
-		</DefaultLayout>
-	);
+  console.log({ session });
+
+  return (
+    <DefaultLayout title="Spotify">
+      <div className={styles.container}>
+        {!session && (
+          <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.name} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </div>
+    </DefaultLayout>
+  );
 }
+
+// export const getServerSideProps = withSession(async function ({ req, res }) {
+//   // Get the user's session based on the request
+//   const user = req.session.get("user");
+
+//   if (!user) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: { user },
+//   };
+// });
