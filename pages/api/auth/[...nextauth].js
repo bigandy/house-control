fb245f2154963c5e0fbabe5b0f2d2f716b9c49a5
Andrew Hudson <ahudson@gmail.com>
2021-03-29 08:31:42 +0100

    a start on next-auth and spotify

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+	// Configure one or more authentication providers
+	providers: [
+		Providers.Spotify({
+			clientId: process.env.SPOTIFY_CLIENT_ID,
+			clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+			// profile(profile) {
+			// 	return {
+			// 		id: profile.id,
+			// 		name: profile.display_name,
+			// 		email: profile.email,
+			// 		image: profile.images?.[0]?.url,
+			// 	};
+			// },
+		}),
+		// ...add more providers here
+	],
+
+	// A database is optional, but required to persist accounts in a database
+	//   database: process.env.DATABASE_URL,
+});
