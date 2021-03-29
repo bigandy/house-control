d9dbc4a411f3e35490d4037c27561503a50affab
Andrew Hudson <andrew@ventureharbour.com>
2021-03-29 13:23:38 +0100

    fix toggleRoom hue function

--- pages/api/hue/toggle-room.ts
+++ pages/api/hue/toggle-room.ts
@@ -1,7 +1,7 @@
 const { toggleRoom } = require("../utils/hue");
 
 export default async function handler(req, res) {
-  const roomId = 0; // Office is id 0
+  const roomId = 1; // Office is id 1
   const on = await toggleRoom(roomId);
 
   res.status(200).json({
