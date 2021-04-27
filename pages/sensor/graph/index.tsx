660422e9bc865438b2249a9a146fd495fa3ea65a
Andrew Hudson <andrew@ventureharbour.com>
2021-04-27 09:13:16 +0100

    changing style. Two graphs not one

--- pages/sensor/graph/index.tsx
+++ pages/sensor/graph/index.tsx
@@ -3,14 +3,14 @@ import { useMemo } from "react";
 import DefaultLayout from "layouts/default";
 
 import { useSensorValuesQuery } from "controllers/sensorValues/hooks";
-import { ScatterPlotCanvas } from "@nivo/scatterplot";
+// import { ScatterPlotCanvas } from "@nivo/scatterplot";
+import { LineCanvas } from "@nivo/line";
 
 const commonProperties = {
   width: 900,
   height: 400,
   margin: { top: 20, right: 20, bottom: 60, left: 40 },
   animate: false,
-  enableSlices: "x",
 };
 
 export default function SensorGraphPage() {
@@ -18,9 +18,8 @@ export default function SensorGraphPage() {
 
   const internal = useMemo(() => {
     if (data?.sensorValues) {
-      return data.sensorValues
-        .filter(({ type }) => type === "inside")
-        .filter((d, i) => i % 9 === 0)
+      return data?.sensorValues
+        .filter(({ type }, i) => type === "inside")
         .map(({ createdAt, temperature, humidity }) => {
           return {
             temperature,
@@ -31,14 +30,11 @@ export default function SensorGraphPage() {
     }
     return [];
   }, [data]);
-
-  console.log(internal.length);
 
   const external = useMemo(() => {
     if (data?.sensorValues) {
-      return data.sensorValues
-        .filter(({ type }) => type === "outside")
-        .filter((d, i) => i % 9 === 0)
+      return data?.sensorValues
+        .filter(({ type }, i) => type === "outside")
         .map(({ createdAt, temperature, humidity }) => {
           return {
             temperature,
@@ -49,56 +45,75 @@ export default function SensorGraphPage() {
     }
     return [];
   }, [data]);
-
-  console.log(external.length);
 
   return (
     <DefaultLayout title="Sensor Graph">
       {data?.sensorValues?.length > 0 && (
-        <ScatterPlotCanvas
-          {...commonProperties}
-          data={[
-            {
-              id: "IN Humidity",
-              data: internal.map((d) => ({
-                x: d.createdAt,
-                y: d.humidity.toFixed(0),
-              })),
-            },
-            {
-              id: "IN Temperature",
-              data: internal.map((d) => ({
-                x: d.createdAt,
-                y: d.temperature.toFixed(0),
-              })),
-            },
+        <div style={{ padding: "0 1rem" }}>
+          <h2>Temperature</h2>
+          <LineCanvas
+            {...commonProperties}
+            data={[
+              {
+                id: "Out Temperature",
+                data: external.map((d) => ({
+                  x: d.createdAt,
+                  y: d?.temperature?.toFixed(0) || null,
+                })),
+              },
+              {
+                id: "IN Temperature",
+                data: internal.map((d) => ({
+                  x: d.createdAt,
+                  y: d.temperature.toFixed(0),
+                })),
+              },
+            ]}
+            xScale={{
+              type: "time",
+              format: "native",
+              precision: "day",
+            }}
+            xFormat="time:%Y-%m-%d"
+            axisBottom={{
+              format: "%b %d",
+              tickValues: "every 2 days",
+            }}
+          />
 
-            {
-              id: "Out Temperature",
-              data: external.map((d) => ({
-                x: d.createdAt,
-                y: d?.temperature?.toFixed(0) || null,
-              })),
-            },
-            {
-              id: "Out Humidity",
-              data: external.map((d) => ({
-                x: d.createdAt,
-                y: d?.humidity?.toFixed(0) || null,
-              })),
-            },
-          ]}
-          xScale={{
-            type: "time",
-            format: "native",
-            precision: "day",
-          }}
-          xFormat="time:%Y-%m-%d"
-          axisBottom={{
-            format: "%b %d",
-            tickValues: "every 2 days",
-          }}
-        />
+          <h2>Humidity</h2>
+
+          <LineCanvas
+            {...commonProperties}
+            data={[
+              {
+                id: "IN Humidity",
+                data: internal.map((d) => ({
+                  x: d.createdAt,
+                  y: d.humidity.toFixed(0),
+                })),
+              },
+
+              {
+                id: "Out Humidity",
+                data: external.map((d) => ({
+                  x: d.createdAt,
+                  y: d?.humidity?.toFixed(0) || null,
+                })),
+              },
+            ]}
+            xScale={{
+              type: "time",
+              format: "native",
+              precision: "day",
+            }}
+            xFormat="time:%Y-%m-%d"
+            axisBottom={{
+              format: "%b %d",
+              tickValues: "every 2 days",
+            }}
+          />
+        </div>
       )}
     </DefaultLayout>
   );
